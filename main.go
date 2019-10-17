242535a36824d821dae7b7a619e384d928b4ac37
hokita <hideee.0202@gmail.com>
2019-10-18 00:26:26 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/nlopes/slack"
+)
+
+// settings of slack
+const (
+	WEBHOOKURL = "[my webhookurl]"
+)
+
+// GetPage function
+func GetPage(url string) {
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("#topics_list4 li").Each(func(_ int, s *goquery.Selection) {
+		imageURL, _ := s.Find("img").Attr("src")
+		url, _ := s.Find(".pic a").Attr("href")
+		shopName := s.Find(".shop-nm .name").Text()
+		text := url + "\n" + shopName
+
+		fmt.Println(text)
+		PostSlack(text, imageURL)
+	})
+}
+
+// PostSlack function
+func PostSlack(text, imageURL string) {
+	payload := &slack.WebhookMessage{
+		Attachments: []slack.Attachment{
+			{
+				Text:     text,
+				ImageURL: imageURL,
+			},
+		},
+	}
+
+	slack.PostWebhook(WEBHOOKURL, payload)
+}
+
+func main() {
+	url := "https://pets-kojima.com/small_list/?topics_group_id=4&group=2&order_type=2"
+	GetPage(url)
+}
